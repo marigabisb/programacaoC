42e74ef613feacf65cae4a79d0da1819d0192756
marigabisb <marigabis18@gmail.com>
2025-04-07 23:39:37 +0000

    criei operacao matematica

--- olamundo.c
+++ olamundo.c
@@ -1,1 +1,43 @@
+    #include <stdio.h>
 
+    int main(){
+        /*
+        Soma (+)
+        Subtracao (-)
+        Multiplicacao (*)
+        Divisao (/)
+        */
+
+    int numero1, numero2;
+    int soma, subtracao, multiplicacao, divisao;
+
+      printf ("Entre com o numero 1: \n");
+      scanf ("%d", &numero1);
+      printf ("Entre com o numero 2: \n");
+      scanf ("%d", &numero2);
+
+      // operacao soma
+    soma = numero1 + numero2;
+
+      // operacao subtracao
+    subtracao = numero1 - numero2;
+
+      // operacao multiplicacao
+    multiplicacao = numero1 * numero2;
+
+      // operacao divisao
+    divisao = numero1 / numero2;
+
+     printf("A Soma é: %d\n", soma);
+     printf("A Subtração é: %d\n", subtracao);
+     printf("A Multiplicação é: %d\n", multiplicacao);
+     printf("A Divisão é: %d\n", divisao);
+
+
+
+
+
+
+
+
+}
